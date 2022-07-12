c8fa18b057319bcc525686e408ede046ccdc4c85
codesolomon <104788657+codesolomon@users.noreply.github.com>
2022-07-12 22:01:30 +0100

    Imported dependencies and components linked

---
+++ App.js
@@ -0,0 +1,47 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Products from "./pages/Products";
+import Error from "./pages/Error";
+import SharedLayout from "./pages/SharedLayout";
+import SingleProduct from "./pages/SingleProduct";
+import Dashboard from "./pages/Dashboard";
+import Login from "./pages/Login";
+import ProtectedRoute from "./pages/protectedRoute";
+import SharedProductLayout from "./pages/SharedProductLayout";
+
+function App() {
+  const [user, setUser] = useState(null);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SharedLayout />}>
+          <Route index element={<Home />} />
+          <Route path="About" element={<About />} />
+
+          <Route path="Products" element={<SharedProductLayout />}>
+            <Route index element={<Products />} />
+            <Route path=":productId" element={<SingleProduct />} />
+          </Route>
+
+          <Route path="/login" element={<Login setUser={setUser}></Login>} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute user={user}>
+                <Dashboard user={user}></Dashboard>
+              </ProtectedRoute>
+            }
+          />
+
+          <Route path="*" element={<Error />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
+
+/* Protected routes are those routes that only grant access to authorized users */
